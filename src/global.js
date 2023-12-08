a20baf2c9fc3563883c052e5e8e24743c8e621b2
04irsyaD <irsyadrisno@gmail.com>
2023-12-08 10:15:54 +0700

    globalv2

--- src/global.js
+++ src/global.js
@@ -1,4 +1,4 @@
-const BASE_DOMAIN = 'http://192.168.8.159:3001'
+const BASE_DOMAIN = 'http://103.93.130.122:4445'
 
 export const BASE_API_URL = `${BASE_DOMAIN}`
 export const BASE_IMAGE_URL = `${BASE_DOMAIN}/asset`
--- src/student_pages/Counseling.jsx
+++ src/student_pages/Counseling.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import "../index.css";
 import { useNavigate } from 'react-router-dom';
 import Navbar from "../components/General/Navbar";
-import profileS from '../components/icon/profile-student.png';
 import axios from "axios";
 import { BASE_API_URL } from '../global.js';
 import { BASE_IMAGE_URL } from '../global.js'
+import iconNotif from '../components/icon/notification-icon.svg';
 
 const SCounseling = () => {
     const [teachers, setTeachers] = useState([]);
@@ -21,17 +21,14 @@ const SCounseling = () => {
                 // Ganti dengan URL API sesuai kebutuhan Anda
                 const token = sessionStorage.getItem('tokeen')
 
-                const response = await axios.get(`${BASE_API_URL}/teacher/getteacher`, {
+                const response = await axios.get(`${BASE_API_URL}/online/getchatguru`, {
                     headers: {
                         Authorization: `Bearer ${token}`
                     }
                 });
-
-                setTcounseling(response.data.data);
+                setTeachers(response.data.data);
                 setLoading(false);
 
-                // Set teachers state dengan data yang diterima dari API
-                setTeachers(response.data);
             } catch (error) {
                 console.error('Error fetching teacher data:', error);
             }
@@ -57,13 +54,16 @@ const SCounseling = () => {
                     <p className="text-base font-poppins text-center">
                         Here is a list of teachers available for online counseling; please choose one.</p>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 pt-10">
-                        {tcounseling.map(teachers => (
+                        {teachers.map(teachers => (
                             <div key={teachers.id_teacher} className="bg-white flex p-4 rounded drop-shadow-lg relative">
                                 <div className="absolute top-[-30px] right-[-30px] p-2">
-
-
+                                    <div className='w-16 h-16 p-2 bg-white drop-shadow-md rounded-full flex items-center justify-center'>
+                                        <img src={iconNotif} alt="Notification" width={30} />
+                                    </div>
+                                    <span className="bg-[#B72024] drop-shadow-sm rounded-full text-white px-2 py-1 text-xs absolute top-4 right-4">
+                                        {teachers.jumlah_chat}
+                                    </span>
                                 </div>
-
                                 <img src={teachers.photo ? `${BASE_IMAGE_URL}/${teachers.photo}` : ''} alt="" width={100} className='pr-3 object-cover object-center w-24 h-32' />
                                 <div className='flex flex-col w-32'>
                                     <h2 className="text-xl font-medium mb-2 overflow-hidden text-[#B72024] font-poppins">
@@ -74,7 +74,7 @@ const SCounseling = () => {
                                 </div>
                                 <button
                                     onClick={() => navigate(`/counseling/${teachers.id_teacher}`, {
-                                        state: { name: teachers.teacher_name, nik: teachers.nik, photo:teachers.photo},
+                                        state: { id_teacher: teachers.id_teacher, name: teachers.teacher_name, nik: teachers.nik, photo: teachers.photo, id_conseling: teachers.id_conseling, },
                                     })} // Sesuaikan rute berdasarkan logika rute aplikasi Anda
                                     className='ml-10 mt-auto px-3 py-1 h-fit rounded-md bg-[#B72024] text-white'>
                                     Start
