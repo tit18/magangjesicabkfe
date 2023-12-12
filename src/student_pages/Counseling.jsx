b285ed04ab588688303a0d583253418a8ca89d5a
whynaa <whynaagustin@gmail.com>
2023-12-12 10:48:34 +0700

    fix online counseling

--- src/student_pages/Counseling.jsx
+++ src/student_pages/Counseling.jsx
@@ -1,54 +1,94 @@
 import React, { useState, useEffect } from 'react';
-import "../index.css";
 import { useNavigate } from 'react-router-dom';
+import { BASE_API_URL, BASE_IMAGE_URL } from '../global.js';
 import Navbar from "../components/General/Navbar";
 import axios from "axios";
-import { BASE_API_URL } from '../global.js';
-import { BASE_IMAGE_URL } from '../global.js'
 import iconNotif from '../components/icon/notification-icon.svg';
+import iconAdd from '../components/icon/plus-circle-fill.svg';
 import Modal from 'react-modal';
+import "../index.css";
 
 const SCounseling = () => {
-    const [teachers, setTeachers] = useState([]);
-    const navigate = useNavigate();
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [teachers, setTeachers] = useState([])
+    const [session, setSession] = useState([])
+    const [selectedTeacher, setSelectedTeacher] = useState("")
+    const [counseling, setCounseling] = useState("")
+    const navigate = useNavigate()
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const fetchTeachers = async () => {
+        try {
+            // Ganti dengan URL API sesuai kebutuhan Anda
+            const token = sessionStorage.getItem('tokeen')
+
+            const response = await axios.get(`${BASE_API_URL}/teacher/getteacher`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            setTeachers(response.data.data);
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const fetchSession = async () => {
+        try {
+            // Ganti dengan URL API sesuai kebutuhan Anda
+            const token = sessionStorage.getItem('tokeen')
+
+            const response = await axios.get(`${BASE_API_URL}/online/getchatguru`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            setSession(response.data.data);
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
 
     useEffect(() => {
-        const fetchTeachers = async () => {
-            try {
-                // Ganti dengan URL API sesuai kebutuhan Anda
-                const token = sessionStorage.getItem('tokeen')
-
-                const response = await axios.get(`${BASE_API_URL}/online/getchatguru`, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                });
-                setTeachers(response.data.data);
-
-            } catch (error) {
-                console.error('Error fetching teacher data:', error);
-            }
-        };
-
-        // Panggil fungsi untuk mengambil data guru
-        fetchTeachers();
+        fetchTeachers()
+        fetchSession()
     }, []);
 
-    console.log('data teacher', teachers)
-    console.log('id', teachers.id_teacher)
-    console.log('photo', teachers.photo)
-
-    const handleResultButtonClick = () => {
-        // Handle the result button click here
-        // For example, open the modal
+    const openModal = () => {
         setIsModalOpen(true);
     };
 
     const closeModal = () => {
-        setIsModalOpen(false);
+        setIsModalOpen(false)
     };
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        try {
+            // Ganti dengan URL API sesuai kebutuhan Anda
+            const token = sessionStorage.getItem('tokeen')
+            const data = {
+                id_teacher: selectedTeacher,
+                counseling: counseling
+            }
+
+            const response = await axios.post(`${BASE_API_URL}/online/addstudent`,
+            data, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            
+            alert(response.data.message)
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+        setIsModalOpen(false)
+
+    }
 
     
     return (
@@ -56,14 +96,14 @@ const SCounseling = () => {
             <div>
                 <Navbar />
             </div>
-            <div className="overflow-x-auto overflow-y flex flex-col items-center justify-center pt-28 gap-4 font-poppins bg-whitef">
+            <div className="overflow-x-auto overflow-y flex flex-col items-center justify-center pt-20 gap-4 font-poppins bg-whitef">
                 <div className="w-full h-fit bg-white shadow-lg py-1 gap-4 flex flex-col items-center justify-center">
                     <h1 className="text-3xl font-bold font-poppins text-center">CURHApps Online Counseling</h1>
                     <p className="text-base font-poppins text-center">
                         Here is a list of teachers available for online counseling; please choose one.
                     </p>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 pt-10">
-                        {teachers.map(teachers => (
+                        {session.map(teachers => (
                             <div key={teachers.id_teacher} className="bg-white flex p-4 rounded drop-shadow-lg relative">
                                 <div className="absolute top-[-30px] right-[-30px] p-2">
                                     <div className='w-16 h-16 p-2 bg-white drop-shadow-md rounded-full flex items-center justify-center'>
@@ -94,9 +134,9 @@ const SCounseling = () => {
                 </div>
             </div>
             {/* Bottom Centered Content */}
-            <div className='fixed bottom-4 right-4'onClick={handleResultButtonClick} >
-                <img src={iconNotif} alt="Notification" width={30} />
-            </div>
+            <button className='fixed bottom-5 right-5'onClick={openModal} >
+                <img src={iconAdd} alt="Add" style={{ fill: 'green' }} width={50} />
+            </button>
             <div className="text-center mt-4 text-xs font-normal text-gray-500 font-Poppins">
                 © {new Date().getFullYear()} Copyright CURHApps All Rights Reserved.
             </div>
@@ -121,29 +161,35 @@ const SCounseling = () => {
                         }}
                     >
                         <div className='flex flex-col p-6'>
-                            <h1 className='text-2xl font-poppins font-bold text-center'>Counseling Result</h1>
-                            <form action="" className='pt-5 h-full '
-                            //onSubmit={handleSubmit}>
-                            >
-                                <label htmlFor="Student" className='text-[#B72024]'>Student</label>
-                                <input
-                                    type="text" // Corrected type
-                                    name=""
-                                    //value={state.name}
-                                    id=""
-                                    className="w-full h-12 px-2 py-3 bg-white border-2 focus:border-black justify-start items-center inline-flex"
-                                    readOnly
-                                />
+                            <h1 className='text-2xl font-poppins font-bold text-center'>Add New Counseling</h1>
+                            <p className='text-sm font-poppins font-light text-center'>you only can have one online session with the same teacher</p>
+                            <form action="" className='pt-5 h-full' onSubmit={handleSubmit}>
 
+                                <label className='text-[#B72024]'>Teacher</label>
+                                <select
+                                    value={selectedTeacher}
+                                    type="text"
+                                    name="id_teacher"
+                                    placeholder="JOHN DOE"
+                                    id="id_teacher"
+                                    className="w-full h-13 px-2 py-3 bg-white border-2 border-neutral-100 justify-start items-center gap-2 inline-flex"
+                                    onChange={(e) => {setSelectedTeacher(e.target.value)}}
+                                    required
+                                >
+                                    <option value="">~~Choose Teacher~~</option>
+                                    {teachers.map((teacher) => (
+                                        <option key={teacher.id_teacher} value={teacher.id_teacher}>
+                                            {teacher.teacher_name}
+                                        </option>
+                                    ))}
+                                </select>
 
-                                <label htmlFor="Result" className='text-[#B72024]'>Counseling Result</label>
+                                <label htmlFor="Result" className='text-[#B72024]'>Counseling Message</label>
                                 <textarea
-                                    name=""
-                                    id=""
                                     placeholder='Text Area'
                                     cols="30"
                                     rows="10"
-                                    //onChange={(e) => setResult(e.target.value)}
+                                    onChange={(e) => setCounseling(e.target.value)}
                                     className='w-full h-32 px-2 py-3 bg-white border-2 focus:border-black justify-start items-center inline-flex'>
                                 </textarea>
 
